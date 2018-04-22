72ee28948a58f0b3e28a77719c66835f9b363ad1
victor <victorrseloy@hotmail.com>
2018-04-22 22:54:42 +0400

    remove unused import

--- src/com/mobiquityinc/packer/Packer.java
+++ src/com/mobiquityinc/packer/Packer.java
@@ -2,7 +2,6 @@ package com.mobiquityinc.packer;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
